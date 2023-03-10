d501c87583f8027e8e12ed11a391f1cbca7e089e
sidneyojr <sidney.o@aluno.ifsp.edu.br>
2023-03-10 21:26:27 +0000

    Adicionando users ao script

--- adduser_IaC.sh
+++ adduser_IaC.sh
@@ -12,3 +12,19 @@ groupadd GRP_ADM
 groupadd GRP_VEN
 groupadd GRP_SEC
 groupadd GRP_PUB
+
+echo " Criando Usuários do Sistema"
+echo ""
+
+useradd carlos -c "Carlos Silva" -s /bin/bash -m -p $(openssl passwd senha1234) -G groupadd GRP_ADM  && passwd guest01 -e
+useradd maria -c "Maria José" -s /bin/bash -m -p $(openssl passwd senha1234) -G groupadd GRP_ADM && passwd guest01 -e
+useradd joao -c "João Carlos" -s /bin/bash -m -p $(openssl passwd senha1234) -G groupadd GRP_ADM && passwd guest01 -e
+
+useradd carlos -c "Carlos" -s /bin/bash -m -p $(openssl passwd senha1234) && passwd guest01 -e
+useradd carlos -c "Carlos" -s /bin/bash -m -p $(openssl passwd senha1234) && passwd guest01 -e
+useradd carlos -c "Carlos" -s /bin/bash -m -p $(openssl passwd senha1234) && passwd guest01 -e
+
+useradd carlos -c "Carlos" -s /bin/bash -m -p $(openssl passwd senha1234) && passwd guest01 -e
+useradd carlos -c "Carlos" -s /bin/bash -m -p $(openssl passwd senha1234) && passwd guest01 -e
+useradd carlos -c "Carlos" -s /bin/bash -m -p $(openssl passwd senha1234) && passwd guest01 -e
+
